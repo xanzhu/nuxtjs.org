be7bae4583ae89f9c4705844407393f187e9e1e2
Alexandre Chopin <alex@orion.sh>
2020-09-29 18:22:12 +0200

    Blog hero

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,47 +1,51 @@
 <template>
-  <div class="shadow-nuxt">
-    <div class="container mx-auto px-4 pt-16 pb-8">
-      <div class="flex flex-wrap justify-between mb-8">
-        <div class="lg:w-6/12 lg:text-left text-center p-4 sm:p-0">
-          <i18n
-            path="blog.title"
-            tag="h1"
-            class="text-3xl xl:text-4xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium leading-normal mb-6 lg:pt-4"
-          >
-            {{ $t('blog.title') }}
-            <template v-slot:nuxt>
-              <AppTitle />
-            </template>
-          </i18n>
+  <div>
+    <TemplateHero>
+      <div class="container mx-auto px-4 pt-16 pb-8">
+        <div class="flex flex-wrap justify-between mb-8">
+          <div class="lg:w-6/12 lg:text-left text-center p-4 sm:p-0">
+            <i18n
+              path="blog.title"
+              tag="h1"
+              class="text-3xl xl:text-4xl text-light-onSurfacePrimary dark:text-dark-onSurfacePrimary font-medium leading-normal mb-6 lg:pt-4"
+            >
+              {{ $t('blog.title') }}
+              <template v-slot:nuxt>
+                <AppTitle />
+              </template>
+            </i18n>
 
-          <!--blog description i18n -->
-          <i18n
-            path="blog.description"
-            tag="h3"
-            class="xl:text-lg light:text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary font-medium leading-relaxed mb-6"
-          >
-            <template v-slot:nuxtTeam>
-              <NuxtLink
-                class="text-nuxt-green underline"
-                to="/team#nuxtCompany"
-              >
-                {{ $t('blog.nuxt_team') }}
-              </NuxtLink>
-            </template>
-            <template v-slot:nuxtCommunity>
-              <NuxtLink
-                class="text-nuxt-green underline"
-                to="/team#nuxtCommunity"
-              >
-                {{ $t('blog.nuxt_community') }}
-              </NuxtLink>
-            </template>
-          </i18n>
+            <!--blog description i18n -->
+            <i18n
+              path="blog.description"
+              tag="h3"
+              class="xl:text-lg light:text-light-onSurfaceSecondary dark:text-dark-onSurfaceSecondary font-medium leading-relaxed mb-6"
+            >
+              <template v-slot:nuxtTeam>
+                <NuxtLink
+                  class="text-nuxt-green underline"
+                  to="/team#nuxtCompany"
+                >
+                  {{ $t('blog.nuxt_team') }}
+                </NuxtLink>
+              </template>
+              <template v-slot:nuxtCommunity>
+                <NuxtLink
+                  class="text-nuxt-green underline"
+                  to="/team#nuxtCommunity"
+                >
+                  {{ $t('blog.nuxt_community') }}
+                </NuxtLink>
+              </template>
+            </i18n>
+          </div>
+          <DesignIllustration
+            class="w-2/3 mx-auto lg:mx-0 lg:w-5/12 lg:-mt-8 text-light-elevatedSurface dark:text-dark-elevatedSurface"
+          />
         </div>
-        <DesignIllustration
-          class="w-2/3 mx-auto lg:mx-0 lg:w-5/12 lg:-mt-8 text-light-elevatedSurface dark:text-dark-elevatedSurface"
-        />
       </div>
+    </TemplateHero>
+    <div class="container mx-auto px-4 pt-16 pb-8">
       <section>
         <BlogpostPreviewItem
           v-for="(post, index) in posts"
